2971b0de823d3ba81b80f5cde93a889683ddd060
Pagina-Web-06 <sheilecalvo6@gmail.com>
2026-07-09 19:37:32 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('wifi-store').then((cache) => cache.addAll(['./caiste.html'])));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
